fbb7f1769eb4ff3f2d4f3b428fc11687f6f78911
Rob <ryoung110400@gmail.com>
2023-02-25 18:11:41 +0000

    Update keybindings

--- lua/kickstart/plugins/keymaps.lua
+++ lua/kickstart/plugins/keymaps.lua
@@ -1,1 +1,5 @@
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<CR>')
+
+vim.keymap.set('n', '<leader>tp', ':BufferPrevious<CR>')
+vim.keymap.set('n', '<leader>tn', ':BufferNext<CR>')
