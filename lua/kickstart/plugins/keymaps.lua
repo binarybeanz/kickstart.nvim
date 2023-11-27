a227d1616f7c2b03c23ad2c2d36c10a96164bd83
rob <ry@jawanet.local>
2023-11-27 16:28:38 +0000

    Add split keymaps

--- lua/kickstart/plugins/keymaps.lua
+++ lua/kickstart/plugins/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<CR>')
 
 vim.keymap.set('n', '<leader>tp', ':BufferPrevious<CR>')
 vim.keymap.set('n', '<leader>tn', ':BufferNext<CR>')
+vim.keymap.set('n', '<leader>tc', ':BufferClose<CR>')
+
+vim.keymap.set('n', '<leader>te', ':vsplit new<CR>')
+vim.keymap.set('n', '<leader>to', ':split new<CR>')
