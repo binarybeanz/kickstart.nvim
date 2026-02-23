7a275030c566bf55fce1d8b7ffacd7489579bab0
bob <bob@binarybeanz.com>
2026-02-23 09:38:45 +0000

    Fix markdown preview build

--- init.lua
+++ init.lua
@@ -156,7 +156,7 @@ require('lazy').setup({
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = ":call mkdp#util#install()",
   },
 
 
